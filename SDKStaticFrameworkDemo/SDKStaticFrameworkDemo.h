e6b7a83408040200a7f1fd1888c177becd3ab84f
郏国上 <jia12216@163.com>
2019-03-06 08:40:59 +0800

    1.0

---
+++ SDKStaticFrameworkDemo/SDKStaticFrameworkDemo.h
@@ -0,0 +1,20 @@
+//
+//  SDKStaticFrameworkDemo.h
+//  SDKStaticFrameworkDemo
+//
+//  Created by 郏国上 on 2019/3/5.
+//  Copyright © 2019年 郏国上. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDKStaticFrameworkDemo.
+FOUNDATION_EXPORT double SDKStaticFrameworkDemoVersionNumber;
+
+//! Project version string for SDKStaticFrameworkDemo.
+FOUNDATION_EXPORT const unsigned char SDKStaticFrameworkDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKStaticFrameworkDemo/PublicHeader.h>
+#import <SDKStaticFrameworkDemo/SDKDemoViewController.h>
+#import <SDKStaticFrameworkDemo/TestTool.h>
+
